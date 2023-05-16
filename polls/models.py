97836c95661332d7d28e9044d69961df44257b55
Octávio Casarini <o.casarini@aluno.ifsp.edu.br>
2023-05-16 11:25:10 -0300

    migrates

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
