b54553329883e99e773030ac8cf94a1720a80ae1
Octávio Casarini <o.casarini@aluno.ifsp.edu.br>
2023-06-06 11:24:20 -0300

    cmuv

--- polls/views.py
+++ polls/views.py
@@ -4,5 +4,16 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
 
-def sobre(request):
-    return HttpResponse("Esta é a página sobre do meu site.")
+def detail(request, question_id):
+    return HttpResponse(f"Essa é a pergunta número {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"Resultados da pergunta de número {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"Você está votando na pergunta {question_id}.")
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
