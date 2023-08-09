395b56b3e022f66b9701e00bc5afa36bc729a57f
Octávio Casarini <octaviocasarini2.0@gmail.com>
2023-08-09 09:31:02 -0300

    updates

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 
 def detail(request, question_id):
-    return HttpResponse(f"Essa é a pergunta número {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse(f"Resultados da pergunta de número {question_id}.")
