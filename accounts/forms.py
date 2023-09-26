6b81d7979c80a90b6a7b84cb46ebc4ecab1d7557
Octávio Casarini <octaviocasarini2.0@gmail.com>
2023-09-26 11:26:38 -0300

    accounts

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class AccountsSignupForm(forms.ModelForm):
+    password = forms.CharField(
+                        label="Senha",
+                        max_length=50,
+                        widget=forms.widgets.PasswordInput()
+                        )
+                        
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path("", views.index, name="index"),
-    # path("<int:question_id>/", views.detail, name="detail"),
-    # path("<int:question_id>/results/", views.results, name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
-
     path('', views.QuestionListView.as_view(), name="question-list"),
     path('cadastrar', views.QuestionCreateView.as_view(), name="question-create"),
     path('<int:pk>', views.QuestionDetailView.as_view(), name="question-detail"),
