6a2d6500a73f9b5f8ed3df2b288f492ca48246c9
Octávio Casarini <octaviocasarini2.0@gmail.com>
2023-11-07 11:08:21 -0300

    votar

--- polls/urls.py
+++ polls/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>/atualizar', views.QuestionUpdateView.as_view(), name="question-update"),
     path('pergunta/<int:pk>/alternativa/add', views.ChoiceCreateView.as_view(), name="choice_add"),
     path('alternativa/<int:pk>/edit', views.ChoiceUpdateView.as_view(), name="choice_edit" ),
-    path('alternativa/<int:pk>/delete', views.ChoiceDeleteView.as_view(), name="choice_delete" )
+    path('alternativa/<int:pk>/delete', views.ChoiceDeleteView.as_view(), name="choice_delete" ),
+    path('pergunta/<int:question_id>/vote', views.vote, name="poll_vote"),
 ] 
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from .models import Question, Choice
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
 
 @login_required
 def detail(request, question_id):
@@ -47,6 +48,14 @@ class QuestionListView(ListView):
 class QuestionDetailView(DetailView):
     model = Question
     context_object_name = 'question'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionDetailView, self).get_context_data(**kwargs)
+        votes = Choice.objects.filter(question=context['question']).aggregate(total=Sum('votes')) or 0
+        context['total_votes'] = votes.get('total')
+
+        return context
+        
 
 class QuestionDeleteView(LoginRequiredMixin, DeleteView):
     model = Question
@@ -138,3 +147,19 @@ class ChoiceDeleteView(LoginRequiredMixin, DeleteView):
         question_id = self.object.question.id
         print(question_id)
         return reverse_lazy('poll_edit', kwargs={'pk': question_id})
+
+def vote (request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        except (KeyError, Choice.DoesNotExist):
+            messages.error(request, 'Selecione uma alternativa para votar!')
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            messages.success(request, 'Seu voto foi registrado com sucesso!')
+            return redirect(reverse_lazy('question-detail', args=(question_id,)))
+
+        context = {'question': question}
+        return render(request, 'polls/question_detail.html', context)
